d89074d73f404d87004be0bbc7baa00e17757781
Allysson Santos <aallysson0@gmail.com>
2019-07-16 20:51:44 -0300

    Updating example stories

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -6,12 +6,16 @@ import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf('Welcome', module).add('to Storybook', () => (
+  <Welcome showApp={linkTo('Button')} />
+));
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
+storiesOf('História do Button', module)
+  .add('Com um texto de Hello Button', () => (
+    <Button onClick={action('HelloButton clicked')}>Hello Button</Button>
+  ))
+  .add('Com alguns emojis', () => (
+    <Button onClick={action('EmojiButton clicked')}>
       <span role="img" aria-label="so cool">
         😀 😎 👍 💯
       </span>
