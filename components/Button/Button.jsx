78c62468e438e2d365dea6f55a9367c68a7d6909
Allysson Santos <aallysson0@gmail.com>
2019-09-07 11:05:42 -0300

    feat(button): add button component

---
+++ components/Button/Button.jsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+import { defaultTheme } from '../../theme';
+
+const Button = styled.button`
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  font-size: 14px;
+  padding: 6px 14px;
+  transition: all 200ms ease-in-out;
+
+  &:hover {
+    box-shadow: 0px 1px 3px 0px #999;
+  }
+
+  ${({ theme, skin }) => `
+    background-color: ${theme.button[skin].bgColor}
+    color: ${theme.button[skin].textColor}
+  `}
+`;
+
+Button.defaultProps = {
+  theme: defaultTheme,
+  skin: 'neutral',
+};
+
+export default Button;
---
+++ stories/button.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { Button } from '../components';
+
+storiesOf('Button', module)
+  .add('Neutral', () => <Button>Hello Button</Button>)
+  .add('Primary', () => <Button skin="primary">Primary Button</Button>)
+  .add('Info', () => <Button skin="info">Info Button</Button>)
+  .add('Success', () => <Button skin="success">Success Button</Button>)
+  .add('Warning', () => <Button skin="warning">Warning Button</Button>)
+  .add('Danger', () => <Button skin="danger">Danger Button</Button>);
