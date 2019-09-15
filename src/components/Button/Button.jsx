4eb6d75f2b3b31fc389ea9b23fae0f762eb9c926
Allysson Santos <aallysson0@gmail.com>
2019-09-15 00:03:24 -0300

    fix(button): fix button box-shadow

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -10,7 +10,7 @@ const Button = styled.button`
   transition: all 200ms ease-in-out;
 
   &:hover {
-    box-shadow: 0px 1px 3px 0px #999;
+    box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.4);
   }
 
   ${({ theme, skin }) => `
