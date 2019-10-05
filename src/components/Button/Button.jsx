b95ca7f1e8ba3575666f116fdfcd2c637122dca2
Allysson Santos <aallysson0@gmail.com>
2019-10-04 21:51:07 -0300

    feat(button): add Icon to Button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,16 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Icon from '@material-ui/core/Icon';
 
 import skins from './skins';
 import defaultTheme from '../../theme';
 
-const Button = styled.button`
+const StyledIcon = styled(Icon)`
+  margin-right: 5px;
+`;
+
+const Btn = styled.button`
   border: none;
   border-radius: 4px;
   cursor: pointer;
@@ -16,19 +22,40 @@ const Button = styled.button`
     box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.4);
   }
 
-  ${({ theme, skin }) => `
+  ${StyledIcon} {
+    font-size: 0.875rem;
+  }
+
+  ${({ theme, skin, icon }) => `
     background-color: ${skins(theme)[skin].bgColor};
     color: ${skins(theme)[skin].textColor};
+    ${icon &&
+      `
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    `}
   `}
 `;
 
+const Button = ({ children, icon, theme, ...props }) => (
+  <Btn icon={icon} theme={theme} {...props}>
+    {icon && <StyledIcon>{icon}</StyledIcon>}
+    {children}
+  </Btn>
+);
+
 Button.propTypes = {
+  children: PropTypes.node.isRequired,
   skin: PropTypes.oneOf(['primary', 'info', 'success', 'danger', 'warning', 'neutral']),
+  icon: PropTypes.string,
+  theme: PropTypes.shape({}),
 };
 
 Button.defaultProps = {
   theme: defaultTheme,
   skin: 'neutral',
+  icon: undefined,
 };
 
 export default Button;
--- stories/button.stories.jsx
+++ stories/button.stories.jsx
@@ -12,3 +12,8 @@ export const success = () => <Button skin="success">Success</Button>;
 export const warning = () => <Button skin="warning">Warning</Button>;
 export const danger = () => <Button skin="danger">Danger</Button>;
 export const neutral = () => <Button skin="neutral">Neutral</Button>;
+export const withIcon = () => (
+  <Button skin="primary" icon="face">
+    With Icon
+  </Button>
+);
