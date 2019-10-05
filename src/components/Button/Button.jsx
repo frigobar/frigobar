d4ce70f7c0f111189587baefffd65fbd0d7adb0f
Allysson Santos <aallysson0@gmail.com>
2019-10-04 22:07:24 -0300

    feat(button): add size prop

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -7,28 +7,48 @@ import skins from './skins';
 import defaultTheme from '../../theme';
 
 const StyledIcon = styled(Icon)`
-  margin-right: 5px;
+  margin-right: ${({ size }) => (size === 'large' ? '8px' : '6px')};
 `;
+
+const ButtonSizes = {
+  small: {
+    fontSize: '0.875rem',
+    iconSize: '1rem',
+    paddings: '6px 14px',
+  },
+  medium: {
+    fontSize: '1rem',
+    iconSize: '1.275rem',
+    paddings: '10px 18px',
+  },
+  large: {
+    fontSize: '1.275rem',
+    iconSize: '1.5rem',
+    paddings: '12px 20px',
+  },
+};
 
 const Btn = styled.button`
   border: none;
   border-radius: 4px;
   cursor: pointer;
   font-size: 0.875rem;
-  padding: 6px 14px;
   transition: all 200ms ease-in-out;
 
   &:hover {
     box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.4);
   }
 
-  ${StyledIcon} {
-    font-size: 0.875rem;
-  }
-
-  ${({ theme, skin, icon }) => `
+  ${({ theme, skin, icon, size }) => `
     background-color: ${skins(theme)[skin].bgColor};
     color: ${skins(theme)[skin].textColor};
+    font-size: ${ButtonSizes[size].fontSize};
+    padding: ${ButtonSizes[size].paddings};
+
+    ${StyledIcon} {
+      font-size: ${ButtonSizes[size].iconSize};
+    }
+
     ${icon &&
       `
       display: flex;
@@ -38,9 +58,9 @@ const Btn = styled.button`
   `}
 `;
 
-const Button = ({ children, icon, theme, ...props }) => (
-  <Btn icon={icon} theme={theme} {...props}>
-    {icon && <StyledIcon>{icon}</StyledIcon>}
+const Button = ({ children, icon, theme, size, ...props }) => (
+  <Btn icon={icon} theme={theme} size={size} {...props}>
+    {icon && <StyledIcon size={size}>{icon}</StyledIcon>}
     {children}
   </Btn>
 );
@@ -50,12 +70,14 @@ Button.propTypes = {
   skin: PropTypes.oneOf(['primary', 'info', 'success', 'danger', 'warning', 'neutral']),
   icon: PropTypes.string,
   theme: PropTypes.shape({}),
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
 };
 
 Button.defaultProps = {
   theme: defaultTheme,
   skin: 'neutral',
   icon: undefined,
+  size: 'small',
 };
 
 export default Button;
