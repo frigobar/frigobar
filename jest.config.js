4ec67e8376552da85ae8a1eb7c616a7b9185e190
Allysson Santos <aallysson0@gmail.com>
2019-10-09 23:05:08 -0300

    test(eslint): update eslint configurations

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   setupFilesAfterEnv: ['./config/setupTest.js'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|ico)$':
+      '<rootDir>/config/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/config/styleMock.js',
+  },
 };
