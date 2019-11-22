7afa7671301ef2c2ecd877b4675901fe9038f6b7
Allysson Santos <aallysson0@gmail.com>
2019-11-22 17:06:35 -0200

    docs(frigobar/storybook): create badge story

---
+++ packages/core/stories/badge.stories.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { Badge } from '../src/components';
+
+export default {
+  title: 'Badge',
+  component: Badge,
+};
+
+export const Default = () => <Badge>10</Badge>;
