25a1d159bebb069eabf3b99b23694983cc613817
Allysson Santos <aallysson0@gmail.com>
2019-11-22 17:59:47 -0200

    improvement(frigobar/badge): add alignment prop to badge

--- packages/core/src/components/Badge/Badge.jsx
+++ packages/core/src/components/Badge/Badge.jsx
@@ -4,6 +4,29 @@ import styled from 'styled-components';
 
 import defaultTheme from '../../theme';
 
+const badgeAlignment = {
+  'top-left': `
+    top: 0;
+    left: 0;
+    transform: translate(-50%, -50%);
+  `,
+  'top-right': `
+    top: 0;
+    right: 0;
+    transform: translate(50%, -50%);
+  `,
+  'bottom-left': `
+    bottom: 0;
+    left: 0;
+    transform: translate(-50%, 50%);
+  `,
+  'bottom-right': `
+    bottom: 0;
+    right: 0;
+    transform: translate(50%, 50%);
+  `,
+};
+
 const Root = styled.span`
   display: inline-flex;
   position: relative;
@@ -11,6 +34,7 @@ const Root = styled.span`
 
 const StyledBadge = styled.span(
   ({
+    alignment,
     color,
     hasChildren,
     theme: {
@@ -31,16 +55,14 @@ const StyledBadge = styled.span(
       hasChildren
         ? `
             position: absolute;
-            right: 0;
-            top: 0;
-            transform: translate(50%, -50%);
+            ${badgeAlignment[alignment]}
           `
         : ''
     }
   `,
 );
 
-const Badge = ({ children, color, theme, content }) => (
+const Badge = ({ children, color, theme, content, alignment }) => (
   <Root>
     {children}
     <StyledBadge
@@ -48,6 +70,7 @@ const Badge = ({ children, color, theme, content }) => (
       color={color}
       role="status"
       hasChildren={Boolean(React.Children.count(children))}
+      alignment={alignment}
     >
       {content}
     </StyledBadge>
@@ -57,11 +80,17 @@ const Badge = ({ children, color, theme, content }) => (
 Badge.defaultProps = {
   theme: defaultTheme,
   color: 'primary',
+  children: undefined,
+  content: undefined,
+  alignment: 'top-right',
 };
 
 Badge.propTypes = {
   theme: PropTypes.shape({}),
   color: PropTypes.oneOf(Object.keys(defaultTheme.colors)),
+  children: PropTypes.node,
+  content: PropTypes.number,
+  alignment: PropTypes.oneOf(['top-left', 'top-right', 'bottom-left', 'bottom-right']),
 };
 
 export default Badge;
