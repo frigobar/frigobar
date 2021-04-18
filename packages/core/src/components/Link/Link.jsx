c1a69ef605443d4d2ef8d6bff9848d5bd1f937c8
Allysson Santos <aallysson0@gmail.com>
2021-04-18 18:07:05 -0300

    refactor(core): remove unnecessary transition

--- packages/core/src/components/Link/Link.jsx
+++ packages/core/src/components/Link/Link.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import defaultTheme from '../../theme';
 
-const Anchor = styled.a`
+const Link = styled.a`
   text-decoration: none;
-  transition: all 0.2s ease-in-out;
 
   ${({
     theme: {
@@ -20,14 +18,7 @@ const Anchor = styled.a`
   `}
 `;
 
-const Link = ({ children, ...props }) => (
-  <Anchor title={children} {...props}>
-    {children}
-  </Anchor>
-);
-
 Link.propTypes = {
-  children: PropTypes.node.isRequired,
   theme: PropTypes.shape({}),
 };
 
