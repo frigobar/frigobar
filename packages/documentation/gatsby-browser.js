67795b051f3f245c42d81a7f62cef9f5548b103b
Allysson Santos <aallysson0@gmail.com>
2021-04-18 01:38:18 -0300

    chore(documentation): remove some dependencies and add web-font-loader plugin

--- packages/documentation/gatsby-browser.js
+++ packages/documentation/gatsby-browser.js
@@ -1,7 +1,3 @@
-// custom typefaces
-import 'typeface-montserrat';
-import 'typeface-merriweather';
-
 import wrap from './wrap-root-element';
 
 export const wrapRootElement = wrap; // eslint-disable-line
