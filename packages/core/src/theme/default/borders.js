b81d3f3321478f1b5dd6ebec3a108fa52ad2cd85
Allysson Santos <aallysson0@gmail.com>
2021-04-18 18:07:25 -0300

    feat(core): add new Borders token

--- packages/core/src/theme/default/borders.js
+++ packages/core/src/theme/default/borders.js
@@ -1,5 +1,5 @@
-const borders = [0, 1];
+const borders = [0, 1, 2, 3];
 
-[borders.none, borders.tiny] = borders;
+[borders.none, borders.tiny, borders.small, borders.medium] = borders;
 
 export default borders;
