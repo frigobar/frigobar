0be8d54ddedfcfb95e079bd16487b228d5355fbb
Allysson Santos <aallysson0@gmail.com>
2021-04-21 19:42:08 -0300

    feat(documentation): update PropTable css styles

--- packages/documentation/src/components/PropsTable/styles.jsx
+++ packages/documentation/src/components/PropsTable/styles.jsx
@@ -11,31 +11,35 @@ const Wrapper = styled.div(
 );
 const Table = styled.table`
   border-collapse: collapse;
-  font-family: monospace;
   margin: 0px 0;
   width: 100%;
 `;
 
 const Thead = styled.thead`
-  font-family: 'Noto Sans JP';
+  font-family: 'Noto Sans JP', sans-serif;
 `;
-const Tbody = styled.tbody``;
 const Th = styled.th`
   border: none;
-  font-size: 14px;
+  font-size: 16px;
   font-weight: 500;
   padding: 14px 12px 12px;
   text-align: left;
 `;
 const Tr = styled.tr``;
+const Tbody = styled.tbody`
+  ${Tr}:nth-child(odd) {
+    background-color: #f6f6f6;
+  }
+`;
 const Td = styled.td(
   ({ theme: { colors } }) => `
     border: none;
     border-top: 1px solid ${colors.neutral[100]};
     padding: 14px 12px 12px;
+    font-size: 14px;
 
-    &:nth-child(3),
-    &:last-child {
+    &:nth-child(n+3) {
+      font-family: 'Fira Code', monospace;
       color: ${colors.primary[500]};
     }
   `,
@@ -43,6 +47,11 @@ const Td = styled.td(
 
 const List = styled.ul`
   padding: 0;
+  margin-left: 20px;
+
+  li {
+    line-height: 20px;
+  }
 `;
 
 export { Wrapper, Table, Thead, Tbody, Th, Tr, Td, List };
