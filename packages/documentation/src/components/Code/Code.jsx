0e9750e1f98482fd415bac070cda001334552c73
Allysson Santos <aallysson0@gmail.com>
2021-06-04 00:10:15 -0300

    feat(documentation): add babel-plugin-styled-components on the react-live example code

--- packages/documentation/src/components/Code/Code.jsx
+++ packages/documentation/src/components/Code/Code.jsx
@@ -1,8 +1,10 @@
+/* eslint-disable global-require */
 import React, { useState, useRef } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import PropTypes from 'prop-types';
 import Highlight, { defaultProps } from 'prism-react-renderer';
 import dracula from 'prism-react-renderer/themes/dracula';
+import { transform } from '@babel/core';
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
 import { MDXContext } from '@mdx-js/react';
 
@@ -22,6 +24,7 @@ const Code = ({
   'react-live': reactLive,
   noInline,
   height,
+  fileName,
 }) => {
   const { name } = useComponent();
 
@@ -31,20 +34,43 @@ const Code = ({
         <MDXContext.Consumer>
           {scope => (
             <LiveProvider
-              scope={{ ...scope, useState, useRef, styled }}
+              scope={{
+                ...scope,
+                useState,
+                useRef,
+                styled,
+                _styled: styled,
+                css,
+              }}
               code={codeString}
               noInline={noInline}
               theme={dracula}
+              transformCode={code => {
+                const transformed = transform(code, {
+                  plugins: [
+                    require('@babel/plugin-syntax-jsx'),
+                    require('babel-plugin-styled-components'),
+                  ],
+                }).code;
+
+                const withoutImport = transformed.replace(
+                  'import _styled from "styled-components";',
+                  '',
+                );
+                const withoutStyled = withoutImport.replace(
+                  /_Styled/g,
+                  'Styled',
+                );
+
+                return withoutStyled;
+              }}
             >
               <ComponentBackground>
                 <LivePreview />
               </ComponentBackground>
 
               <EditorBackground>
-                <Tab>
-                  {name}
-                  .jsx
-                </Tab>
+                <Tab>{fileName || `${name}.jsx`}</Tab>
                 <LiveEditor style={{ height: '100%', fontSize: 14 }} />
               </EditorBackground>
               <Error>
@@ -86,6 +112,7 @@ Code.propTypes = {
   'react-live': PropTypes.string,
   noInline: PropTypes.bool,
   height: PropTypes.string,
+  fileName: PropTypes.string,
 };
 
 Code.defaultProps = {
@@ -94,6 +121,7 @@ Code.defaultProps = {
   'react-live': undefined,
   noInline: false,
   height: '500',
+  fileName: undefined,
 };
 
 export default Code;
