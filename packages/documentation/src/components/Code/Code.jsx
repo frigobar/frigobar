fa03218a634257d83d4bb8183667831c76eaeb31
Allysson Santos <aallysson0@gmail.com>
2021-06-08 21:35:31 -0300

    fix(documentation): add try/catch to avoid breaking app, switch editor/preview panels

--- packages/documentation/src/components/Code/Code.jsx
+++ packages/documentation/src/components/Code/Code.jsx
@@ -3,7 +3,7 @@ import React, { useState, useRef } from 'react';
 import styled, { css } from 'styled-components';
 import PropTypes from 'prop-types';
 import Highlight, { defaultProps } from 'prism-react-renderer';
-import dracula from 'prism-react-renderer/themes/dracula';
+import dracula from 'prism-react-renderer/themes/nightOwl';
 import { transform } from '@babel/core';
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
 import { MDXContext } from '@mdx-js/react';
@@ -43,43 +43,50 @@ const Code = ({
                 css,
               }}
               code={codeString}
+              language={language}
               noInline={noInline}
               theme={dracula}
               transformCode={code => {
-                if (
-                  typeof document !== 'undefined' &&
-                  typeof window !== 'undefined'
-                ) {
-                  const transformed = transform(code, {
-                    plugins: [
-                      require('@babel/plugin-syntax-jsx'),
-                      require('babel-plugin-styled-components'),
-                    ],
-                  }).code;
+                try {
+                  if (
+                    typeof document !== 'undefined' &&
+                    typeof window !== 'undefined'
+                  ) {
+                    const transformed = transform(code, {
+                      plugins: [
+                        require('@babel/plugin-syntax-jsx'),
+                        require('babel-plugin-styled-components'),
+                      ],
+                    }).code;
 
-                  const withoutImport = transformed.replace(
-                    'import _styled from "styled-components";',
-                    '',
-                  );
-                  const withoutStyled = withoutImport.replace(
-                    /_Styled/g,
-                    'Styled',
-                  );
+                    const withoutImport = transformed.replace(
+                      'import _styled from "styled-components";',
+                      '',
+                    );
+                    const withoutStyled = withoutImport.replace(
+                      /_Styled/g,
+                      'Styled',
+                    );
 
-                  return withoutStyled;
+                    return withoutStyled;
+                  }
+                } catch (e) {
+                  return code;
                 }
 
-                return undefined;
+                return code;
               }}
             >
+              <EditorBackground>
+                <Tab>{fileName || `${name}.jsx`}</Tab>
+                <LiveEditor
+                  style={{ height: '100%', fontSize: 14 }}
+                  className="live-editor"
+                />
+              </EditorBackground>
               <ComponentBackground>
                 <LivePreview />
               </ComponentBackground>
-
-              <EditorBackground>
-                <Tab>{fileName || `${name}.jsx`}</Tab>
-                <LiveEditor style={{ height: '100%', fontSize: 14 }} />
-              </EditorBackground>
               <Error>
                 <LiveError />
               </Error>
