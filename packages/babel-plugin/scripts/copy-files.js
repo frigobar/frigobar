f7bfebfd772e9f0a0977a7500a1eb10ba9091422
Allysson Santos <aallysson0@gmail.com>
2021-06-10 22:25:37 -0300

    build(babel-plugin): add copy-files script

---
+++ packages/babel-plugin/scripts/copy-files.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const fs = require('fs');
+const pkg = require('../package.json');
+
+const copyFile = file => {
+  const buildPath = path.resolve(__dirname, '../dist/', path.basename(file));
+  fs.copyFile(
+    file,
+    buildPath,
+    () => console.log(`Copied ${file} to ${buildPath}`), // eslint-disable-line no-console
+  );
+};
+
+const createPackageJson = () => {
+  const {
+    scripts,
+    devDependencies,
+    'lint-staged': lintStaged,
+    jest,
+    config,
+    release,
+    ...packageDataOther
+  } = pkg;
+
+  const newPackageData = {
+    ...packageDataOther,
+    main: './index.js',
+    private: false,
+  };
+
+  const buildPath = path.resolve(__dirname, '../dist/package.json');
+
+  fs.writeFileSync(buildPath, JSON.stringify(newPackageData, null, 2), 'utf8');
+  console.log(`Created package.json in ${buildPath}`); // eslint-disable-line no-console
+
+  return newPackageData;
+};
+
+const run = () => {
+  ['LICENSE', 'README.md'].map(copyFile);
+  createPackageJson();
+};
+
+run();
