31916070631ef9ef39b84f49a56eddc75674e717
Allysson Santos <aallysson0@gmail.com>
2021-07-06 20:10:48 -0300

    feat(core): edit eslint and add ts config

--- packages/core/testUtils.tsx
+++ packages/core/testUtils.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { render, fireEvent, waitFor } from '@testing-library/react';
+import {
+  render,
+  fireEvent,
+  waitFor,
+  RenderOptions,
+  RenderResult,
+} from '@testing-library/react';
 import { ThemeProvider } from 'styled-components';
 
 import theme from './src/theme';
 
-const Providers = ({ children }) => {
+const Providers = ({ children }: { children?: React.ReactNode }) => {
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 };
 
@@ -13,8 +19,10 @@ Providers.propTypes = {
   children: PropTypes.node.isRequired,
 };
 
-const customRender = (ui, options) =>
-  render(ui, { wrapper: Providers, ...options });
+const customRender = (
+  ui: React.ReactElement,
+  options?: Omit<RenderOptions, 'queries'>,
+): RenderResult => render(ui, { wrapper: Providers, ...options });
 
 // re-export everything
 export * from '@testing-library/react';
