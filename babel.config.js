a1e1268b951983cc2441971cd15ceeef4265fdee
Allysson Santos <aallysson0@gmail.com>
2021-07-06 19:57:08 -0300

    fix(root): add typescript and edit eslint configuration

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  presets: [
+    '@babel/preset-react',
+    '@babel/preset-env',
+    '@babel/preset-typescript',
+  ],
+  plugins: ['inline-react-svg'],
   env: {
     production: {
       ignore: ['./**/*.test.js', './**/*.test.jsx'],
