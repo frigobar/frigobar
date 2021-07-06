aef8548976f4c2af6f7269511999b5685c7ec6d8
Allysson Santos <aallysson0@gmail.com>
2021-07-06 19:57:46 -0300

    chore(core): remove cli component creation helper

--- packages/core/scripts/cli/creators/index.js
+++
@@ -1,5 +0,0 @@
-const createComponent = require('./component');
-const createTest = require('./test');
-const createStory = require('./story');
-
-module.exports = { createComponent, createTest, createStory };
--- packages/core/scripts/cli/utils/index.js
+++
@@ -1,6 +0,0 @@
-const getPaths = require('./getPaths');
-const createDirectory = require('./createDirectory');
-const createFile = require('./createFile');
-const editIndex = require('./editIndex');
-
-module.exports = { getPaths, createDirectory, createFile, editIndex };
