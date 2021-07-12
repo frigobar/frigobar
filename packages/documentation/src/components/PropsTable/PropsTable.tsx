dbabc851e2fa00789176ef72aab6271d002d47ed
Allysson Santos <aallysson0@gmail.com>
2021-07-12 09:42:03 -0300

    refactor(documentation): remove proptypes import from PropsTable component

--- packages/documentation/src/components/PropsTable/PropsTable.tsx
+++ packages/documentation/src/components/PropsTable/PropsTable.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import { Wrapper, Table, Thead, Tbody, Th, Tr, Td } from './styles';
 import { usePropsTable } from './PropsTableContext';
