b9c7f4cba418d45bc4d56e8f47bdc06db307736d
Allysson Santos <aallysson0@gmail.com>
2021-12-22 23:09:33 -0300

    feat(core): export Modal component

--- packages/core/src/components/index.ts
+++ packages/core/src/components/index.ts
@@ -5,5 +5,6 @@ import Card from './Card';
 import Link from './Link';
 import Menu from './Menu';
 import ThemeProvider from './ThemeProvider';
+import Modal from './Modal';
 
-export { Alert, Badge, Button, Card, Link, Menu, ThemeProvider };
+export { Alert, Badge, Button, Card, Link, Menu, ThemeProvider, Modal };
