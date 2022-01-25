5bef9f5bc07660550e241b1051a8f1449f84196b
Allysson Santos <aallysson0@gmail.com>
2022-01-25 00:26:26 -0300

    feat(core): add outline prop to Button component

--- packages/core/src/components/Button/Button.tsx
+++ packages/core/src/components/Button/Button.tsx
@@ -13,8 +13,7 @@ interface ButtonProps {
   full?: boolean;
   /** add border-radius circle */
   rounded?: boolean;
-  /** large button */
-  large?: boolean;
+  outline?: boolean;
 }
 
 const sizes = ({ spacings }: DefaultTheme) => ({
@@ -58,65 +57,65 @@ const Button = styled.button<ButtonProps>(
     disabled,
     full,
     rounded,
-    large,
+    outline,
   }) => {
     const { radius, colors } = theme;
 
     return css`
-  font-size: 0.875rem;
-  font-size: ${large ? '1.125' : sizes(theme)[size].font}rem;
+      font-size: 0.875rem;
+      font-size: ${sizes(theme)[size].font}rem;
 
-  padding: ${
-    large
-      ? `
-          ${sizes(theme)[size].padding.top * 2}px
-          ${sizes(theme)[size].padding.right * 2}px
-          ${sizes(theme)[size].padding.bottom * 2}px
-          ${sizes(theme)[size].padding.left * 2}px
-      `
-      : `
-          ${sizes(theme)[size].padding.top}px
-          ${sizes(theme)[size].padding.right}px
-          ${sizes(theme)[size].padding.bottom}px
-          ${sizes(theme)[size].padding.left}px
-      `
-  };
+      padding: ${sizes(theme)[size].padding.top}px
+        ${sizes(theme)[size].padding.right}px
+        ${sizes(theme)[size].padding.bottom}px
+        ${sizes(theme)[size].padding.left}px;
 
-  cursor: pointer;
+      cursor: pointer;
 
-  transition: all 200ms ease-in-out;
+      transition: all 200ms ease-in-out;
 
-  text-decoration: none;
+      text-decoration: none;
 
-  color: ${colors.neutral[50]};
-  border: none;
-  border-radius: 4px;
-  
-  background-color: ${colors[skin][500]};
+      color: ${colors.neutral[50]};
+      border: 2px solid transparent;
+      border-radius: 4px;
+      
+      background-color: ${colors[skin][800]};
 
-    ${
-      disabled
-        ? css`
-            cursor: not-allowed;
-            color: ${colors.neutral[400]};
-            background-color: ${colors.neutral[100]};
-          `
-        : css`
-            &:hover {
-              box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.4);
-            }
-          `
-    }
+      ${full ? 'width: 100%;' : ''}
+      ${
+        rounded
+          ? css`
+              border-radius: ${radius[3]}px;
+            `
+          : ''
+      }
 
-    ${full ? 'width: 100%;' : ''}
-    ${
-      rounded
-        ? css`
-            border-radius: ${radius[3]}px;
-          `
-        : ''
-    }
-`;
+      ${
+        outline
+          ? css`
+              background-color: ${colors[skin][50]};
+              color: ${colors[skin][900]};
+              border-color: ${colors[skin][900]};
+            `
+          : ''
+      }
+
+      ${
+        disabled
+          ? css`
+              cursor: not-allowed;
+              color: ${colors.neutral[400]};
+              background-color: ${colors.neutral[100]};
+              border-color: transparent;
+            `
+          : css`
+              &:hover {
+                box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.4);
+              }
+            `
+      }
+    `;
   },
 );
 
@@ -126,7 +125,6 @@ Button.defaultProps = {
   size: 'small',
   full: undefined,
   rounded: undefined,
-  large: undefined,
 };
 
 export default Button;
