b02c7e39eda77ef6ce949167490d8a6d43609383
Allysson Santos <aallysson0@gmail.com>
2022-04-28 00:25:54 -0300

    test(babel-plugin): add rest parameter to unit tests

--- packages/babel-plugin/tests/fixtures/arrayMultiple/code.js
+++ packages/babel-plugin/tests/fixtures/arrayMultiple/code.js
@@ -1,11 +1,16 @@
 function App(props) {
   const animation = 'dsadsa ddsad dsad';
   const animation2 = 'test';
+  const rest = {
+    'data-foo': 'foo',
+    'data-bar': 'bar',
+  };
   return (
     <div
       animation={[animation, animation2]}
       otherProp="otherProp"
       newProp="newProp"
+      {...rest}
     >
       Hello world
     </div>
--- packages/babel-plugin/tests/fixtures/arraySingle/code.js
+++ packages/babel-plugin/tests/fixtures/arraySingle/code.js
@@ -1,7 +1,16 @@
 function App(props) {
   const animation = 'test';
+  const rest = {
+    'data-foo': 'foo',
+    'data-bar': 'bar',
+  };
   return (
-    <div animation={[animation]} otherProp="otherProp" newProp="newProp">
+    <div
+      animation={[animation]}
+      otherProp="otherProp"
+      newProp="newProp"
+      {...rest}
+    >
       Hello world
     </div>
   );
